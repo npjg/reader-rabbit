62fe7cf219e6f8e0616a486a1ff598c180539e32
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2021-03-23 14:44:56 -0400

    Initial commit

---
+++ df.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+
+import argparse
+import logging
+import struct
+import os
+from pathlib import Path
+import glob
+import mmap
+import io
+import subprocess
+import uuid
+
+class Object:
+    def __format__(self, spec):
+        return self.__repr__()
+
+# Observed chunks:
+# FNT0
+# ANG\x00
+# 
+
+class Chunk(Object):
+    def __init__(self, stream):
+        self.id = stream.read(0x04).replace(b'\x00', b'').decode("utf-8")
+        unk1 = struct.unpack("<L", stream.read(4))[0]
+        unk2 = struct.unpack("<L", stream.read(4))[0]
+        unk3 = struct.unpack("<L", stream.read(4))[0]
+        # assert unk1 == unk2
+
+        assert stream.read(0x04) == b'COMP'
+        unk4 = struct.unpack("<L", stream.read(4))[0]
+        unk5 = struct.unpack("<L", stream.read(4))[0]
+        # assert unk3 == unk4
+
+        self.length = struct.unpack("<L", stream.read(4))[0]
+        data = stream.read(self.length)
+
+class CDChunk(Object):
+    def __init__(self, stream):
+        self.id = stream.read(0x04).replace(b'\x00', b'').decode("utf-8")
+        unk1 = struct.unpack("<L", stream.read(4))[0] # 00 00
+        unk2 = struct.unpack("<L", stream.read(4))[0] # 10 00
+        self.length = struct.unpack("<L", stream.read(4))[0]
+
+        if self.id == 'KWAV':
+            self.chunk = KWAV(stream)
+        else:
+            self.chunk = stream.read(self.length)
+
+class KWAV(Object):
+    def __init__(self, stream):
+        assert stream.read(4) == b'KWAV'
+        length = struct.unpack("<L", stream.read(4))[0]
+        self.data = stream.read(length)
+
+    def export(self, directory, filename=None):
+        if not filename:
+            filename = "{}-{}".format("KWAV", str(uuid.uuid4()))
+
+        command = ['ffmpeg', '-y', '-f', 's16le', '-ar', '11.025k', '-ac', '1', '-i', 'pipe:', os.path.join(directory, "{}.wav".format(filename))]
+        with subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) as p:
+            p.stdin.write(self.data)
+
+def process(filename):
+    logging.debug("Processing file: {}".format(filename))
+    if args.export:
+        Path(args.export).mkdir(parents=True, exist_ok=True)
+
+    with open(filename, mode='rb') as f:
+        stream = mmap.mmap(f.fileno(), length=0, access=mmap.ACCESS_READ)
+        assert stream.read(4) == b'\x44\x46\x00\x00'
+
+        chunks = []
+        # stream.seek(0xea6c)
+        # stream.seek(0x5dcc)
+        # stream.seek(0x1d5cc39)
+        # stream.seek(0x5e96e8)
+        stream.seek(0x010882)
+        chunk_ids = {}
+        try:
+            while True:
+                start = stream.tell()
+                chunk = CDChunk(stream)
+
+                if not chunk_ids.get(chunk.id):
+                    chunk_ids.update({chunk.id: 0})
+
+                chunk_ids[chunk.id] += 1
+                print("(@0x{:012x}) Chunk: {} (0x{:08x} bytes)".format(start, chunk.id, chunk.length))
+                if args.export:
+                    if callable(getattr(chunk.chunk, "export", None)):
+                        chunk.chunk.export(args.export, "{}-{}".format(chunk.id, chunk_ids[chunk.id]))
+
+        except Exception as e:
+            logging.error("Exception at {}:{:012x}".format(filename, stream.tell()))
+            raise
+
+def main():
+    process(args.input)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="jumpstart", formatter_class=argparse.RawTextHelpFormatter,
+         description="""Parse asset structures and extract assets from RR interactive titles."""
+    )
+
+    parser.add_argument(
+        "input", help="Pass a DF filename to process the file."
+    )
+
+    parser.add_argument(
+        "export", nargs='?', default=None,
+        help="Specify the location for exporting assets, or omit to skip export."
+    )
+
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG)
+    main()
